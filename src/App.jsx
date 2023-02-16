88d3d0bf8ae08061034c8f0b1c7f563b1c470531
DeeptiSaxenaRai <deepti.kodehode@gmail.com>
2023-02-16 23:42:55 +0100

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import LandingPage from "./routes/LandingPage";
 import AboutPage from "./routes/about/AboutPage";
